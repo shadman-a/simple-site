50c18bbb737ca978b8fb76cce9413edc40d682ac
shadman-a <shadman@outlook.com>
2020-10-01 17:38:16 -0400

    something

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { Component } from 'react';
 
-const Resume = () => {
-  return (
-    <div>
-      <h1>Resume Dann</h1>
-    </div>
-  )
-}
+class Resume extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Resume</h1>
+        <button onClick={this.handleSubmit}>Clivk</button>
+      </div>
+    )
+  } 
+ }
+
 
 export default Resume 
