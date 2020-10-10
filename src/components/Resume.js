87af1e70db8480cfafa0214f0061c38f55e104de
Shadman Ahmed <61790489+shadman-a@users.noreply.github.com>
2020-10-09 20:33:58 -0400

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,7 +5,7 @@ class Resume extends Component {
   render() {
     return (
       <div>
-        <h1>Resume :
+        <h1>Resume:
         </h1>
         <button onClick={this.handleSubmit}>Click</button>
       </div>
